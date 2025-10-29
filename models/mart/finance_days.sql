023461d335827332bd275c87f73cc5cae089d930
jamesigthain@gmail.com <jamesigthain@gmail.com>
2025-10-29 15:10:14 +0000

    for finance team all values aggreagated by date_date

---
+++ models/mart/finance_days.sql
@@ -0,0 +1,14 @@
+SELECT
+date_date
+,COUNT (orders_id) AS transactions
+,SUM (revenue) AS revenue
+,AVG(quantity) AS avg_items_basket
+,AVG(revenue) AS avg_basket_revenue
+,SUM(operational_margin) AS operational_margin
+,SUM(purchase_cost) AS purchase_cost
+,SUM(shipping_fee) AS shipping_fee
+,sum(logcost) AS logcost
+,SUM(quantity) AS produc_sold
+FROM {{ ref('int_orders_operational') }}
+GROUP BY date_date
+ORDER BY date_date ASC
