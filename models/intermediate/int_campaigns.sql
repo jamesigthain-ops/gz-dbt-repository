25b4c276f05598e0413477a05bc9be3a8b6b8612
jamesigthain@gmail.com <jamesigthain@gmail.com>
2025-10-30 15:33:06 +0000

    union all corrected with new imported data package thingy

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,15 +1,18 @@
-SELECT
-*
-FROM {{ ref('stg_raw__facebook') }}
-UNION ALL 
-SELECT
-*
-FROM {{ ref('stg_raw__criteo') }}
-UNION ALL 
-SELECT
-*
-FROM {{ ref('stg_raw__bing') }}
-UNION ALL 
-SELECT
-*
-FROM {{ ref('stg_raw__adwords') }}
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__facebook'),
+        ref('stg_raw__criteo')
+    ],
+    include=[
+        'date_date',
+        'paid_source',
+        'campaign_key',
+        'campgn_name',
+        'ads_cost',
+        'impression',
+        'click',
+    ],
+    source_column_name=None
+) }}
