d3519e37560d4f1155ca70073e9c9b40ff387c37
jamesigthain@gmail.com <jamesigthain@gmail.com>
2025-10-30 15:47:39 +0000

    better union all combo thingy

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -4,15 +4,5 @@
         ref('stg_raw__bing'),
         ref('stg_raw__facebook'),
         ref('stg_raw__criteo')
-    ],
-    include=[
-        'date_date',
-        'paid_source',
-        'campaign_key',
-        'campgn_name',
-        'ads_cost',
-        'impression',
-        'click',
-    ],
-    source_column_name=None
+    ]
 ) }}
